ecdd40c77c218b3528e6f80e0ecbd54334ead9bd
Ahmed Abu Lawi <ahmedlawi92@gmail.com>
2018-07-22 21:40:31 -0400

    Use a css normalizer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import App from "./js/App";
 import ReactDOM from 'react-dom'
 import React from 'react';
+import { injectGlobal } from 'emotion'
+
+injectGlobal`
+    ${require('raw-loader!normalize.css')}
+`;
 
 const wrapper = document.getElementById("react-root");
 wrapper ? ReactDOM.render(<App />, wrapper) : false;
